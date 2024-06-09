76bb46389d31aeafa3bbfb5fdbc5d6e489941d85
Yesh Dattatreya <ydatta@amazon.com>
2024-06-09 07:33:43 -0700

    Enlarged the model and fixed a bug with the last linear layer's input dimension

--- train.py
+++ train.py
@@ -4,12 +4,13 @@ from torch.nn import functional as F
 torch.manual_seed(1337)
 
 class Head(nn.Module):
-    def __init__(self, head_size, embedding_dim, block_size):
+    def __init__(self, head_size, embedding_dim, block_size, dropout_rate):
         super().__init__()
         self.key = nn.Linear(embedding_dim, head_size, bias=False)
         self.query = nn.Linear(embedding_dim, head_size, bias=False)
         self.value = nn.Linear(embedding_dim, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout_rate)
 
     def forward(self, x):
         # Formula from the Attention all you need paper is:
@@ -24,60 +25,73 @@ class Head(nn.Module):
         # we need the [:T, :T] slice because during inference, the sequence length can be less than block_size
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
         wei = F.softmax(wei, dim=-1) # (B, T, T)
+        wei = self.dropout(wei)
         # perform the weighted aggregation of the values
         out = wei @ v # (B, T, T) @ (B, T, head_size) -> (B, T, head_size)
         return out
 
 class MultHeadAttention(nn.Module):
-    def __init__(self, num_heads, head_size, embedding_dim, block_size):
+    def __init__(self, num_heads, head_size, embedding_dim, block_size, dropout_rate):
         super().__init__()
         # each head is batch_size B x block_size T x head_size
-        self.heads = nn.ModuleList([Head(head_size, embedding_dim, block_size) for _ in range(num_heads)])
+        self.heads = nn.ModuleList([Head(head_size, embedding_dim, block_size, dropout_rate) for _ in range(num_heads)])
         self.proj = nn.Linear(head_size * num_heads, embedding_dim)
+        self.dropout = nn.Dropout(dropout_rate)
 
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1) # (B, T, head_size * num_heads)
-        out = self.proj(out) # (B, T, C)
+        out = self.proj(out) # (B, T, C) - project back to embedding dim
+        out = self.dropout(out) # (B, T, C)
         return out
 
 class FeedForward(nn.Module):
-    def __init__(self, embedding_dim):
+    def __init__(self, embedding_dim, dropout_rate):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(embedding_dim, 4 * embedding_dim),
             nn.ReLU(),
             nn.Linear(4 * embedding_dim, embedding_dim),
+            nn.Dropout(dropout_rate),
         )
 
     def forward(self, x):
         return self.net(x)
 
 class Block(nn.Module):
-    def __init__(self, embedding_dim, head_size, num_heads, block_size):
+    def __init__(self, embedding_dim, head_size, num_heads, block_size, dropout_rate):
         super().__init__()
-        self.sa_heads = MultHeadAttention(num_heads, head_size//num_heads, embedding_dim, block_size) # (B, T, head_size)
-        self.ffwd = FeedForward(embedding_dim) # (B, T, C)
+        self.sa_heads = MultHeadAttention(num_heads, head_size//num_heads, embedding_dim, block_size, dropout_rate) # (B, T, head_size)
+        self.ffwd = FeedForward(embedding_dim, dropout_rate) # (B, T, C)
+        self.ln1 = nn.LayerNorm(embedding_dim)
+        self.ln2 = nn.LayerNorm(embedding_dim)
 
     def forward(self, x):
-        x = x + self.sa_heads(x) # added residual connection. Size is (B, T, C)
-        x = x + self.ffwd(x) # added residual connection. Size is (B, T, C)
+        # Apply layernorm 1 to the input of the heads (before attention)  and layernorm 2 to input of FF layer (output of attention)
+        x = x + self.sa_heads(self.ln1(x)) # first add a residual connection to the output of the attention. Size is (B, T, C)
+        x = x + self.ffwd(self.ln2(x)) # then add another residual connection from output of head to the output of the FF layer. Size is (B, T, C)
         return x
 
 class BigramLanguageModel(nn.Module):
 
-    def __init__(self, vocab_size, block_size, head_size, num_heads, embedding_dim=64):
+    def __init__(self, vocab_size, block_size, head_size, num_heads, num_layers, embedding_dim, dropout_rate):
         super().__init__()
         self.block_size = block_size
         self.token_embedding_table = nn.Embedding(vocab_size, embedding_dim)
         self.position_embedding_table = nn.Embedding(block_size, embedding_dim)
         # self.sa_heads = MultHeadAttention(num_heads, head_size//num_heads, embedding_dim, block_size) # (B, T, head_size)
         # self.ffwd = FeedForward(embedding_dim) # (B, T, C)
+        # Sequential doesn't accept a list, so we need to use * to decompose the list
         self.blocks = nn.Sequential(
-            Block(embedding_dim, head_size, num_heads, block_size),
-            Block(embedding_dim, head_size, num_heads, block_size),
-            Block(embedding_dim, head_size, num_heads, block_size),
+            *[Block(embedding_dim, head_size, num_heads, block_size, dropout_rate) for _ in range(num_layers)]
         )
-        self.lm_head = nn.Linear(head_size, vocab_size) # (B, T, C)
+        # self.blocks = nn.Sequential(
+        #     Block(embedding_dim, head_size, num_heads, block_size),
+        #     Block(embedding_dim, head_size, num_heads, block_size),
+        #     Block(embedding_dim, head_size, num_heads, block_size),
+        #     nn.LayerNorm(embedding_dim),
+        # )
+        self.ln_final = nn.LayerNorm(embedding_dim)
+        self.lm_head = nn.Linear(embedding_dim, vocab_size) # (B, T, C)
         
     def forward(self, idx, targets=None):
         # idx is (batch_size, block_size) tensor of integers
@@ -90,6 +104,7 @@ class BigramLanguageModel(nn.Module):
         # x = self.sa_heads(x) # (batch_size, block_size, head_size)
         # x = self.ffwd(x) # (batch_size, block_size, C)
         x = self.blocks(x)
+        x = self.ln_final(x)
         logits = self.lm_head(x) # (batch_size, block_size, vocab_size)
         loss = None
         if targets is not None:
@@ -143,16 +158,20 @@ def get_batch(data, block_size, batch_size):
 def train():
     train_data, val_data, vocab_size, encode, decode = get_data()
     
-    block_size = 8
-    batch_size = 32
-    n_embeddings = 32
-    head_size = 32
-    num_heads = 4
+    block_size = 256
+    batch_size = 64
+    n_embeddings = 64*4
+    head_size = 12*6
+    num_heads = 6
+    num_layers = 6
+    dropout_rate = 0.2
+    learning_rate = 3e-4
+
     max_iters = 5000
     eval_iters = 1000
     
-    m = BigramLanguageModel(vocab_size, block_size, head_size, num_heads, n_embeddings)
-    optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
+    m = BigramLanguageModel(vocab_size, block_size, head_size, num_heads, num_layers, n_embeddings, dropout_rate)
+    optimizer = torch.optim.AdamW(m.parameters(), lr=learning_rate)
 
     for iter in range(max_iters):
         # sample a batch of data
